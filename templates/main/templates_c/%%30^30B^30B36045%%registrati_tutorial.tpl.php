af006b72cf3e18a8b9cb45825d74713078638dd6
michele <michele.taranta22@libero.it>
2015-08-04 11:43:08 +0200

    .....

---
+++ templates/main/templates_c/%%30^30B^30B36045%%registrati_tutorial.tpl.php
@@ -0,0 +1,67 @@
+<?php /* Smarty version 2.6.13, created on 2015-08-04 10:49:58
+         compiled from registrati_tutorial.tpl */ ?>
+<div>
+  <!-- One -->
+  <section id="one" class="wrapper style2 special">
+    <section class="spotlight">
+      <div class="image"><img src="images/registrazione.jpg" alt="" /></div>
+      <div class="content">
+          <h2>1) Registrati su FantaConTe<br> </h2>
+      </div>
+    </section>
+    
+    <!-- Two -->
+    
+    <section id="two" class="wrapper style2 special">
+      <section class="spotlight">
+	<div class="image"><img src="images/mercato.jpg" alt="" /></div>
+        <div class="content">
+	  <h2>2) Crea la tua squadra perfetta <br>
+	      nel mercato della Serie A</h2>
+	    <p>Potrai scegliere una squadra di 25 giocatori della Serie A<br>
+	       con 3 Portieri, 8 Difensori, 8 Centrocampisti e 6 Attaccanti.</p>
+	</div>
+      </section>
+    </section>
+    
+    <!-- Three -->
+    
+    <section id="three" class="wrapper style2 special">
+      <section class="spotlight">
+	<div class="image"><img src="images/modulo.jpg" alt="" /></div>
+        <div class="content">
+	  <h2>3) Inserisci il modulo della tua squadra </h2>
+	    <p>Potrai scegliere i giocatori Titolari<br />
+	        e i giocatori Panchinari.</p>
+	</div>
+      </section>
+    </section>
+
+    <!-- Four -->
+				
+    <section id="four" class="wrapper style2 special">
+      <section class="spotlight">
+	<div class="image"><img src="images/ciao.jpg" alt="" /></div>
+          <div class="content">
+	    <h2>4) Sfida i tuoi amici</h2>
+	      <p>Aspetta la prossima giornata di Serie A<br />
+		 e sconfiggi i tuoi avversari.</p>
+	  </div>
+      </section>
+    </section>
+
+    <!-- CTA -->
+    
+    <section id="cta" class="wrapper style4">
+      <div class="inner">
+	<header>
+	  <h2>Fine Tutorial</h2>
+	  <p>Entra nel sito e gioca...</p>
+	</header>
+	<ul class="actions vertical">
+	  <li><a href="#" class="button fit special">Torna Su</a></li>
+	</ul>
+      </div>
+    </section>
+  </section>
+					</div>
